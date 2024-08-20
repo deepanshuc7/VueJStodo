67df372d8dfc5a9a57ba89db96d6687b3986b85b
Deepanshu Chaudhary <61338791+deepanshuc7@users.noreply.github.com>
2024-08-20 23:08:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VueJStodo/',
+  // base: '/VueJStodo/',
   plugins: [vue()],
 })
 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === "production" ? "/VueJStodo/" : "/",
-// };
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/VueJStodo/" : "/",
+};
